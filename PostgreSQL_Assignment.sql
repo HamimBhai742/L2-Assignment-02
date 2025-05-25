ad3e30d54db9b2c6899c67f2261a5a314a7cef3e
HamimBhai742 <omagootruelove147@gmail.com>
2025-05-25 12:11:05 +0600

    Insert data into sightings table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -32,7 +32,7 @@ create table sightings(
 INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'Central Plains'),
-('Charlie Black', 'Southern Woods'),
+('Charlie Black', 'River Delta'),
 ('Diana Blue', 'Eastern Forests'),
 ('Ethan Gray', 'Western Mountains'),
 ('Carol King','Mountain Range');
@@ -47,3 +47,18 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 (' Snow Leopard ', 'Panthera uncia', '1775-02-14', 'Endangered'),
 ('Grizzly Bear', 'Ursus arctos horribilis', '1825-09-05', 'Vulnerable'),
 ('American Bison', 'Bison bison', '1780-04-18', 'Endangered');
+
+--Insert data into sightings table
+INSERT INTO sightings (ranger_id, species_id, sighting_time, location, notes) VALUES
+(3, 4, '2024-05-18 18:30:00 ', 'Peak Ridge', 'Spotted near the riverbank'),
+(1, 2, '2024-05-18 18:30:00', 'Bankwood Area', 'Observed during a patrol'),
+(4, 3, '2024-05-18 18:30:00', 'Bamboo Grove East', 'Seen grazing in the meadow'),
+(3, 2, '2024-05-18 18:30:00', 'Snowfall Pass', 'Noted for its vibrant fur color'),
+(1, 4, '2024-05-18 18:30:00', 'Western Mountains', NULL),
+(6, 3, '2024-05-18 18:30:00', 'Mountain Range', 'Rare sighting in this area'),
+(2, 1, '2024-05-18 18:30:00', 'Central Plains', 'Observed during a routine check'),
+(3, 5, '2024-05-18 18:30:00', 'Northern Hills', NULL),
+(2, 6, '2024-05-18 18:30:00', 'River Delta Pass', NULL),
+(4, 7, '2024-05-18 18:30:00', 'Eastern Forests', NULL),
+(3, 8, '2024-05-18 18:30:00', 'Western Mountains Pass', 'Observed during a routine check');
+
